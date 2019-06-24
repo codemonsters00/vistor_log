a3ebce603379c4076dd75aaaa54d9a94032c8951
codemonsters00 <51632829+codemonsters00@users.noreply.github.com>
2019-06-24 12:02:36 +0000

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(title:Text("Vistor Log"),),
+        ));
+  }
+}
